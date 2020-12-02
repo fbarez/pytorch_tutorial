64487b7351952ff2716c618bc741dc8039b34653
Fazl Barez <fazlbarez93@gmail.com>
2020-12-02 16:54:39 +0000

    chapter one torch book

---
+++ utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plot_learning_curve
+
+
+def plot_learning_curves(x, scores, epsilons, filename):
+    fig = plot.figure()
+    ax = fig.add_subplot(111, label='1')
+    ax = fig.add_subplot(111, label='2', frame_on=False)
+
+
+    ax.plot(x, epsilion, color="C0")
+    ax.set_xlabel("Training Steps", color="C0")
+    ax.set_ylabel("Epsilion", color="C0")
+    ax.tick_params(axis='x', color="C0")
+    ax.tick_params(axis='y', color="C0")
+
+
+    N = len(scores)
+
+    running_avg = np.empty(N)
+    for t in range(N):
+        running_avg[t] = np.mean(scores[max(0, t-100):(t+1)])
+
+
+        ax2.scatter(x, running_avg, color="C1")
+        ax2.axes.get_xaxis().set_visible(False)
+        ax2.yaxis.tick_right()
+        ax2.set_ylabel('Score', color="C1")
+        ax2.yaxis.set_label_position('right')
+        ax2.tick_params(axis='y', color="C1")
+
+        plt.savefig(filename)
